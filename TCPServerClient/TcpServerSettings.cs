c72af13abe2207822dc340f21553400d5e7a9efd
Khánh Hoàng <bacdau852@gmail.com>
2023-04-04 22:05:02 +0700

    TcpServerSettings: Clean Up

--- TCPServerClient/TcpServerSettings.cs
+++ TCPServerClient/TcpServerSettings.cs
@@ -48,41 +48,6 @@ namespace TcpServerClient
 		}
 
 		/// <summary>
-		/// Maximum amount of time to wait before considering a client idle and disconnecting them. 
-		/// By default, this value is set to 0, which will never disconnect a client due to inactivity.
-		/// The timeout is reset any time a message is received from a client.
-		/// For instance, if you set this value to 30000, the client will be disconnected if the server has not received a message from the client within 30 seconds.
-		/// </summary>
-		public int IdleClientTimeoutMs
-		{
-			get
-			{
-				return _idleClientTimeoutMs;
-			}
-			set
-			{
-				if (value < 0) throw new ArgumentException("IdleClientTimeoutMs must be zero or greater.");
-				_idleClientTimeoutMs = value;
-			}
-		}
-
-		/// <summary>
-		/// Number of milliseconds to wait between each iteration of evaluating connected clients to see if they have exceeded the configured timeout interval.
-		/// </summary>
-		public int IdleClientEvaluationIntervalMs
-		{
-			get
-			{
-				return _idleClientEvaluationIntervalMs;
-			}
-			set
-			{
-				if (value < 1) throw new ArgumentOutOfRangeException("IdleClientEvaluationIntervalMs must be one or greater.");
-				_idleClientEvaluationIntervalMs = value;
-			}
-		}
-
-		/// <summary>
 		/// Enable or disable whether the data receiver thread fires the DataReceived event from a background task.
 		/// The default is enabled.
 		/// </summary>
@@ -94,8 +59,6 @@ namespace TcpServerClient
 
 		private bool _noDelay = false;
 		private int _streamBufferSize = 65536;
-		private int _idleClientTimeoutMs = 0;
-		private int _idleClientEvaluationIntervalMs = 5000;
 
 		#endregion
 
