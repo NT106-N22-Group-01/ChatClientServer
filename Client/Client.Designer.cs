dc15c0d6c81da2915f715e9729b17e26665a4a0f
tuilakhanh <bacdau852@gmail.com>
2023-04-06 21:09:28 +0700

    Client: Done

--- Client/Client.Designer.cs
+++ Client/Client.Designer.cs
@@ -1,205 +1,225 @@
 ﻿namespace Client
 {
-    partial class Client
-    {
-        /// <summary>
-        /// Required designer variable.
-        /// </summary>
-        private System.ComponentModel.IContainer components = null;
+	partial class Client
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
 
-        /// <summary>
-        /// Clean up any resources being used.
-        /// </summary>
-        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
-        protected override void Dispose(bool disposing)
-        {
-            if (disposing && (components != null))
-            {
-                components.Dispose();
-            }
-            base.Dispose(disposing);
-        }
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
 
-        #region Windows Form Designer generated code
+		#region Windows Form Designer generated code
 
-        /// <summary>
-        /// Required method for Designer support - do not modify
-        /// the contents of this method with the code editor.
-        /// </summary>
-        private void InitializeComponent()
-        {
-            this.label1 = new System.Windows.Forms.Label();
-            this.serverIPTxt = new System.Windows.Forms.TextBox();
-            this.label2 = new System.Windows.Forms.Label();
-            this.serverPortTxt = new System.Windows.Forms.TextBox();
-            this.clientConnectButton = new System.Windows.Forms.Button();
-            this.clientDisconnectButton = new System.Windows.Forms.Button();
-            this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.label3 = new System.Windows.Forms.Label();
-            this.nameTxt = new System.Windows.Forms.TextBox();
-            this.clientChatView = new System.Windows.Forms.RichTextBox();
-            this.msgTxt = new System.Windows.Forms.TextBox();
-            this.clientSendButton = new System.Windows.Forms.Button();
-            this.groupBox2 = new System.Windows.Forms.GroupBox();
-            this.SuspendLayout();
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Arial", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(50, 50);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(83, 18);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "IP Address";
-            // 
-            // serverIPTxt
-            // 
-            this.serverIPTxt.Location = new System.Drawing.Point(50, 80);
-            this.serverIPTxt.Name = "serverIPTxt";
-            this.serverIPTxt.Size = new System.Drawing.Size(100, 20);
-            this.serverIPTxt.TabIndex = 1;
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Arial", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(190, 50);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(38, 18);
-            this.label2.TabIndex = 2;
-            this.label2.Text = "Port";
-            // 
-            // serverPortTxt
-            // 
-            this.serverPortTxt.Location = new System.Drawing.Point(190, 80);
-            this.serverPortTxt.Name = "serverPortTxt";
-            this.serverPortTxt.Size = new System.Drawing.Size(100, 20);
-            this.serverPortTxt.TabIndex = 3;
-            // 
-            // clientConnectButton
-            // 
-            this.clientConnectButton.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.clientConnectButton.Location = new System.Drawing.Point(450, 50);
-            this.clientConnectButton.Name = "clientConnectButton";
-            this.clientConnectButton.Size = new System.Drawing.Size(90, 50);
-            this.clientConnectButton.TabIndex = 4;
-            this.clientConnectButton.Text = "Connect";
-            this.clientConnectButton.UseVisualStyleBackColor = true;
-            // 
-            // clientDisconnectButton
-            // 
-            this.clientDisconnectButton.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.clientDisconnectButton.Location = new System.Drawing.Point(570, 50);
-            this.clientDisconnectButton.Name = "clientDisconnectButton";
-            this.clientDisconnectButton.Size = new System.Drawing.Size(90, 50);
-            this.clientDisconnectButton.TabIndex = 5;
-            this.clientDisconnectButton.Text = "Disconnect";
-            this.clientDisconnectButton.UseVisualStyleBackColor = true;
-            // 
-            // groupBox1
-            // 
-            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.groupBox1.Location = new System.Drawing.Point(30, 30);
-            this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(650, 80);
-            this.groupBox1.TabIndex = 6;
-            this.groupBox1.TabStop = false;
-            this.groupBox1.Text = "Connect to Server";
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Arial", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(320, 50);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(48, 18);
-            this.label3.TabIndex = 7;
-            this.label3.Text = "Name";
-            // 
-            // nameTxt
-            // 
-            this.nameTxt.Location = new System.Drawing.Point(320, 80);
-            this.nameTxt.Name = "nameTxt";
-            this.nameTxt.Size = new System.Drawing.Size(100, 20);
-            this.nameTxt.TabIndex = 8;
-            // 
-            // clientChatView
-            // 
-            this.clientChatView.Location = new System.Drawing.Point(30, 120);
-            this.clientChatView.Name = "clientChatView";
-            this.clientChatView.Size = new System.Drawing.Size(650, 300);
-            this.clientChatView.TabIndex = 9;
-            this.clientChatView.Text = "";
-            // 
-            // msgTxt
-            // 
-            this.msgTxt.Location = new System.Drawing.Point(50, 470);
-            this.msgTxt.Multiline = true;
-            this.msgTxt.Name = "msgTxt";
-            this.msgTxt.Size = new System.Drawing.Size(500, 50);
-            this.msgTxt.TabIndex = 10;
-            // 
-            // clientSendButton
-            // 
-            this.clientSendButton.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.clientSendButton.Location = new System.Drawing.Point(570, 470);
-            this.clientSendButton.Name = "clientSendButton";
-            this.clientSendButton.Size = new System.Drawing.Size(90, 50);
-            this.clientSendButton.TabIndex = 11;
-            this.clientSendButton.Text = "Send";
-            this.clientSendButton.UseVisualStyleBackColor = true;
-            // 
-            // groupBox2
-            // 
-            this.groupBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.groupBox2.Location = new System.Drawing.Point(30, 450);
-            this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(650, 90);
-            this.groupBox2.TabIndex = 12;
-            this.groupBox2.TabStop = false;
-            this.groupBox2.Text = "Message";
-            // 
-            // Client
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(704, 561);
-            this.Controls.Add(this.clientSendButton);
-            this.Controls.Add(this.msgTxt);
-            this.Controls.Add(this.clientChatView);
-            this.Controls.Add(this.nameTxt);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.clientDisconnectButton);
-            this.Controls.Add(this.clientConnectButton);
-            this.Controls.Add(this.serverPortTxt);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.serverIPTxt);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.groupBox1);
-            this.Controls.Add(this.groupBox2);
-            this.Name = "Client";
-            this.Text = "Client";
-            this.ResumeLayout(false);
-            this.PerformLayout();
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			label1 = new System.Windows.Forms.Label();
+			serverIPTxt = new System.Windows.Forms.TextBox();
+			label2 = new System.Windows.Forms.Label();
+			serverPortTxt = new System.Windows.Forms.TextBox();
+			clientConnectButton = new System.Windows.Forms.Button();
+			clientDisconnectButton = new System.Windows.Forms.Button();
+			groupBox1 = new System.Windows.Forms.GroupBox();
+			label3 = new System.Windows.Forms.Label();
+			nameTxt = new System.Windows.Forms.TextBox();
+			clientChatView = new System.Windows.Forms.RichTextBox();
+			msgTxt = new System.Windows.Forms.TextBox();
+			clientSendButton = new System.Windows.Forms.Button();
+			groupBox2 = new System.Windows.Forms.GroupBox();
+			SuspendLayout();
+			// 
+			// label1
+			// 
+			label1.AutoSize = true;
+			label1.Font = new System.Drawing.Font("Arial", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+			label1.Location = new System.Drawing.Point(83, 96);
+			label1.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+			label1.Name = "label1";
+			label1.Size = new System.Drawing.Size(130, 27);
+			label1.TabIndex = 0;
+			label1.Text = "IP Address";
+			// 
+			// serverIPTxt
+			// 
+			serverIPTxt.Location = new System.Drawing.Point(83, 154);
+			serverIPTxt.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
+			serverIPTxt.Name = "serverIPTxt";
+			serverIPTxt.Size = new System.Drawing.Size(164, 31);
+			serverIPTxt.TabIndex = 1;
+			// 
+			// label2
+			// 
+			label2.AutoSize = true;
+			label2.Font = new System.Drawing.Font("Arial", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+			label2.Location = new System.Drawing.Point(317, 96);
+			label2.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+			label2.Name = "label2";
+			label2.Size = new System.Drawing.Size(58, 27);
+			label2.TabIndex = 2;
+			label2.Text = "Port";
+			// 
+			// serverPortTxt
+			// 
+			serverPortTxt.Location = new System.Drawing.Point(317, 154);
+			serverPortTxt.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
+			serverPortTxt.Name = "serverPortTxt";
+			serverPortTxt.Size = new System.Drawing.Size(164, 31);
+			serverPortTxt.TabIndex = 3;
+			// 
+			// clientConnectButton
+			// 
+			clientConnectButton.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+			clientConnectButton.Location = new System.Drawing.Point(750, 96);
+			clientConnectButton.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
+			clientConnectButton.Name = "clientConnectButton";
+			clientConnectButton.Size = new System.Drawing.Size(150, 96);
+			clientConnectButton.TabIndex = 4;
+			clientConnectButton.Text = "Connect";
+			clientConnectButton.UseVisualStyleBackColor = true;
+			clientConnectButton.Click += clientConnectButton_Click;
+			// 
+			// clientDisconnectButton
+			// 
+			clientDisconnectButton.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+			clientDisconnectButton.Location = new System.Drawing.Point(950, 96);
+			clientDisconnectButton.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
+			clientDisconnectButton.Name = "clientDisconnectButton";
+			clientDisconnectButton.Size = new System.Drawing.Size(150, 96);
+			clientDisconnectButton.TabIndex = 5;
+			clientDisconnectButton.Text = "Disconnect";
+			clientDisconnectButton.UseVisualStyleBackColor = true;
+			clientDisconnectButton.Click += clientDisconnectButton_Click;
+			// 
+			// groupBox1
+			// 
+			groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+			groupBox1.Location = new System.Drawing.Point(50, 58);
+			groupBox1.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
+			groupBox1.Name = "groupBox1";
+			groupBox1.Padding = new System.Windows.Forms.Padding(5, 6, 5, 6);
+			groupBox1.Size = new System.Drawing.Size(1083, 154);
+			groupBox1.TabIndex = 6;
+			groupBox1.TabStop = false;
+			groupBox1.Text = "Connect to Server";
+			// 
+			// label3
+			// 
+			label3.AutoSize = true;
+			label3.Font = new System.Drawing.Font("Arial", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+			label3.Location = new System.Drawing.Point(533, 96);
+			label3.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+			label3.Name = "label3";
+			label3.Size = new System.Drawing.Size(74, 27);
+			label3.TabIndex = 7;
+			label3.Text = "Name";
+			// 
+			// nameTxt
+			// 
+			nameTxt.Location = new System.Drawing.Point(533, 154);
+			nameTxt.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
+			nameTxt.Name = "nameTxt";
+			nameTxt.Size = new System.Drawing.Size(164, 31);
+			nameTxt.TabIndex = 8;
+			// 
+			// clientChatView
+			// 
+			clientChatView.Location = new System.Drawing.Point(50, 231);
+			clientChatView.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
+			clientChatView.Name = "clientChatView";
+			clientChatView.Size = new System.Drawing.Size(1081, 573);
+			clientChatView.TabIndex = 9;
+			clientChatView.Text = "";
+			// 
+			// msgTxt
+			// 
+			msgTxt.Location = new System.Drawing.Point(83, 904);
+			msgTxt.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
+			msgTxt.Multiline = true;
+			msgTxt.Name = "msgTxt";
+			msgTxt.Size = new System.Drawing.Size(831, 92);
+			msgTxt.TabIndex = 10;
+			msgTxt.TextChanged += msgTxt_TextChanged;
+			// 
+			// clientSendButton
+			// 
+			clientSendButton.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+			clientSendButton.Location = new System.Drawing.Point(950, 904);
+			clientSendButton.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
+			clientSendButton.Name = "clientSendButton";
+			clientSendButton.Size = new System.Drawing.Size(150, 96);
+			clientSendButton.TabIndex = 11;
+			clientSendButton.Text = "Send";
+			clientSendButton.UseVisualStyleBackColor = true;
+			clientSendButton.Click += clientSendButton_Click;
+			// 
+			// groupBox2
+			// 
+			groupBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+			groupBox2.Location = new System.Drawing.Point(50, 865);
+			groupBox2.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
+			groupBox2.Name = "groupBox2";
+			groupBox2.Padding = new System.Windows.Forms.Padding(5, 6, 5, 6);
+			groupBox2.Size = new System.Drawing.Size(1083, 173);
+			groupBox2.TabIndex = 12;
+			groupBox2.TabStop = false;
+			groupBox2.Text = "Message";
+			// 
+			// Client
+			// 
+			AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+			AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			ClientSize = new System.Drawing.Size(1173, 1079);
+			Controls.Add(clientSendButton);
+			Controls.Add(msgTxt);
+			Controls.Add(clientChatView);
+			Controls.Add(nameTxt);
+			Controls.Add(label3);
+			Controls.Add(clientDisconnectButton);
+			Controls.Add(clientConnectButton);
+			Controls.Add(serverPortTxt);
+			Controls.Add(label2);
+			Controls.Add(serverIPTxt);
+			Controls.Add(label1);
+			Controls.Add(groupBox1);
+			Controls.Add(groupBox2);
+			Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
+			Name = "Client";
+			Text = "Client";
+			Load += Client_Load;
+			ResumeLayout(false);
+			PerformLayout();
+		}
 
-        }
+		#endregion
 
-        #endregion
-
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.TextBox serverIPTxt;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.TextBox serverPortTxt;
-        private System.Windows.Forms.Button clientConnectButton;
-        private System.Windows.Forms.Button clientDisconnectButton;
-        private System.Windows.Forms.GroupBox groupBox1;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.TextBox nameTxt;
-        private System.Windows.Forms.RichTextBox clientChatView;
-        private System.Windows.Forms.TextBox msgTxt;
-        private System.Windows.Forms.Button clientSendButton;
-        private System.Windows.Forms.GroupBox groupBox2;
-    }
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.TextBox serverIPTxt;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.TextBox serverPortTxt;
+		private System.Windows.Forms.Button clientConnectButton;
+		private System.Windows.Forms.Button clientDisconnectButton;
+		private System.Windows.Forms.GroupBox groupBox1;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.TextBox nameTxt;
+		private System.Windows.Forms.RichTextBox clientChatView;
+		private System.Windows.Forms.TextBox msgTxt;
+		private System.Windows.Forms.Button clientSendButton;
+		private System.Windows.Forms.GroupBox groupBox2;
+	}
 }
 
