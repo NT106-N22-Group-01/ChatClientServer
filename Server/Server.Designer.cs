50feef84e729040411187e730b833b064e818bb5
NhatGit <116144245+NhatGit@users.noreply.github.com>
2023-04-06 21:08:30 +0700

    Server: Done logic side

--- Server/Server.Designer.cs
+++ Server/Server.Designer.cs
@@ -1,131 +1,131 @@
 ﻿namespace Server
 {
-    partial class Server
-    {
-        /// <summary>
-        /// Required designer variable.
-        /// </summary>
-        private System.ComponentModel.IContainer components = null;
+	partial class Server
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
 
-        /// <summary>
-        /// Clean up any resources being used.
-        /// </summary>
-        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
-        protected override void Dispose(bool disposing)
-        {
-            if (disposing && (components != null))
-            {
-                components.Dispose();
-            }
-            base.Dispose(disposing);
-        }
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
 
-        #region Windows Form Designer generated code
+		#region Windows Form Designer generated code
 
-        /// <summary>
-        /// Required method for Designer support - do not modify
-        /// the contents of this method with the code editor.
-        /// </summary>
-        private void InitializeComponent()
-        {
-			this.label1 = new System.Windows.Forms.Label();
-			this.serverPortTxt = new System.Windows.Forms.TextBox();
-			this.serverStartButton = new System.Windows.Forms.Button();
-			this.serverStopButton = new System.Windows.Forms.Button();
-			this.groupBox1 = new System.Windows.Forms.GroupBox();
-			this.serverChatView = new System.Windows.Forms.RichTextBox();
-			this.SuspendLayout();
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			label1 = new System.Windows.Forms.Label();
+			serverPortTxt = new System.Windows.Forms.TextBox();
+			serverStartButton = new System.Windows.Forms.Button();
+			serverStopButton = new System.Windows.Forms.Button();
+			groupBox1 = new System.Windows.Forms.GroupBox();
+			serverChatView = new System.Windows.Forms.RichTextBox();
+			SuspendLayout();
 			// 
 			// label1
 			// 
-			this.label1.AutoSize = true;
-			this.label1.Font = new System.Drawing.Font("Arial", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.label1.Location = new System.Drawing.Point(75, 77);
-			this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-			this.label1.Name = "label1";
-			this.label1.Size = new System.Drawing.Size(58, 27);
-			this.label1.TabIndex = 0;
-			this.label1.Text = "Port";
+			label1.AutoSize = true;
+			label1.Font = new System.Drawing.Font("Arial", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+			label1.Location = new System.Drawing.Point(83, 96);
+			label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+			label1.Name = "label1";
+			label1.Size = new System.Drawing.Size(58, 27);
+			label1.TabIndex = 0;
+			label1.Text = "Port";
 			// 
 			// serverPortTxt
 			// 
-			this.serverPortTxt.Location = new System.Drawing.Point(75, 123);
-			this.serverPortTxt.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
-			this.serverPortTxt.Name = "serverPortTxt";
-			this.serverPortTxt.Size = new System.Drawing.Size(148, 26);
-			this.serverPortTxt.TabIndex = 1;
+			serverPortTxt.Location = new System.Drawing.Point(83, 154);
+			serverPortTxt.Margin = new System.Windows.Forms.Padding(4, 6, 4, 6);
+			serverPortTxt.Name = "serverPortTxt";
+			serverPortTxt.Size = new System.Drawing.Size(164, 31);
+			serverPortTxt.TabIndex = 1;
 			// 
 			// serverStartButton
 			// 
-			this.serverStartButton.Font = new System.Drawing.Font("Arial", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.serverStartButton.Location = new System.Drawing.Point(270, 77);
-			this.serverStartButton.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
-			this.serverStartButton.Name = "serverStartButton";
-			this.serverStartButton.Size = new System.Drawing.Size(150, 77);
-			this.serverStartButton.TabIndex = 2;
-			this.serverStartButton.Text = "Start";
-			this.serverStartButton.UseVisualStyleBackColor = true;
-			this.serverStartButton.Click += new System.EventHandler(this.serverStartButton_Click);
+			serverStartButton.Font = new System.Drawing.Font("Arial", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+			serverStartButton.Location = new System.Drawing.Point(300, 96);
+			serverStartButton.Margin = new System.Windows.Forms.Padding(4, 6, 4, 6);
+			serverStartButton.Name = "serverStartButton";
+			serverStartButton.Size = new System.Drawing.Size(167, 96);
+			serverStartButton.TabIndex = 2;
+			serverStartButton.Text = "Start";
+			serverStartButton.UseVisualStyleBackColor = true;
+			serverStartButton.Click += serverStartButton_Click;
 			// 
 			// serverStopButton
 			// 
-			this.serverStopButton.Font = new System.Drawing.Font("Arial", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.serverStopButton.Location = new System.Drawing.Point(465, 77);
-			this.serverStopButton.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
-			this.serverStopButton.Name = "serverStopButton";
-			this.serverStopButton.Size = new System.Drawing.Size(150, 77);
-			this.serverStopButton.TabIndex = 3;
-			this.serverStopButton.Text = "Stop";
-			this.serverStopButton.UseVisualStyleBackColor = true;
+			serverStopButton.Font = new System.Drawing.Font("Arial", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+			serverStopButton.Location = new System.Drawing.Point(517, 96);
+			serverStopButton.Margin = new System.Windows.Forms.Padding(4, 6, 4, 6);
+			serverStopButton.Name = "serverStopButton";
+			serverStopButton.Size = new System.Drawing.Size(167, 96);
+			serverStopButton.TabIndex = 3;
+			serverStopButton.Text = "Stop";
+			serverStopButton.UseVisualStyleBackColor = true;
+			serverStopButton.Click += serverStopButton_Click;
 			// 
 			// groupBox1
 			// 
-			this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.groupBox1.Location = new System.Drawing.Point(45, 46);
-			this.groupBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
-			this.groupBox1.Name = "groupBox1";
-			this.groupBox1.Padding = new System.Windows.Forms.Padding(4, 5, 4, 5);
-			this.groupBox1.Size = new System.Drawing.Size(600, 138);
-			this.groupBox1.TabIndex = 4;
-			this.groupBox1.TabStop = false;
-			this.groupBox1.Text = "Start Server";
+			groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+			groupBox1.Location = new System.Drawing.Point(50, 58);
+			groupBox1.Margin = new System.Windows.Forms.Padding(4, 6, 4, 6);
+			groupBox1.Name = "groupBox1";
+			groupBox1.Padding = new System.Windows.Forms.Padding(4, 6, 4, 6);
+			groupBox1.Size = new System.Drawing.Size(667, 172);
+			groupBox1.TabIndex = 4;
+			groupBox1.TabStop = false;
+			groupBox1.Text = "Start Server";
 			// 
 			// serverChatView
 			// 
-			this.serverChatView.Location = new System.Drawing.Point(45, 231);
-			this.serverChatView.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
-			this.serverChatView.Name = "serverChatView";
-			this.serverChatView.Size = new System.Drawing.Size(973, 582);
-			this.serverChatView.TabIndex = 5;
-			this.serverChatView.Text = "";
+			serverChatView.Location = new System.Drawing.Point(50, 289);
+			serverChatView.Margin = new System.Windows.Forms.Padding(4, 6, 4, 6);
+			serverChatView.Name = "serverChatView";
+			serverChatView.Size = new System.Drawing.Size(1081, 726);
+			serverChatView.TabIndex = 5;
+			serverChatView.Text = "";
 			// 
 			// Server
 			// 
-			this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
-			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(1056, 863);
-			this.Controls.Add(this.serverChatView);
-			this.Controls.Add(this.serverStopButton);
-			this.Controls.Add(this.serverStartButton);
-			this.Controls.Add(this.serverPortTxt);
-			this.Controls.Add(this.label1);
-			this.Controls.Add(this.groupBox1);
-			this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
-			this.Name = "Server";
-			this.Text = "Server";
-			this.ResumeLayout(false);
-			this.PerformLayout();
+			AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+			AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			ClientSize = new System.Drawing.Size(1173, 1079);
+			Controls.Add(serverChatView);
+			Controls.Add(serverStopButton);
+			Controls.Add(serverStartButton);
+			Controls.Add(serverPortTxt);
+			Controls.Add(label1);
+			Controls.Add(groupBox1);
+			Margin = new System.Windows.Forms.Padding(4, 6, 4, 6);
+			Name = "Server";
+			Text = "Server";
+			ResumeLayout(false);
+			PerformLayout();
+		}
 
-        }
+		#endregion
 
-        #endregion
-
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.TextBox serverPortTxt;
-        private System.Windows.Forms.Button serverStartButton;
-        private System.Windows.Forms.Button serverStopButton;
-        private System.Windows.Forms.GroupBox groupBox1;
-        private System.Windows.Forms.RichTextBox serverChatView;
-    }
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.TextBox serverPortTxt;
+		private System.Windows.Forms.Button serverStartButton;
+		private System.Windows.Forms.Button serverStopButton;
+		private System.Windows.Forms.GroupBox groupBox1;
+		private System.Windows.Forms.RichTextBox serverChatView;
+	}
 }
 
--- Server/Server.cs
+++ Server/Server.cs
@@ -1,25 +1,28 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using TcpServerClient;
+using Data;
+using System.Collections.Concurrent;
 
 namespace Server
 {
-    public partial class Server : Form
-    {
-        public Server()
-        {
-            InitializeComponent();
-        }
+	public partial class Server : Form
+	{
+		private STcpServer _server = null;
+		private readonly ConcurrentDictionary<string, string> _usernames = new ConcurrentDictionary<string, string>();
+
+		private delegate void UpdateStatusDelegate(string status);
+		private UpdateStatusDelegate _updateStatusDelegate;
+
+		public Server()
+		{
+			InitializeComponent();
+			serverStopButton.Enabled = false;
+		}
 
 		private void serverStartButton_Click(object sender, EventArgs e)
 		{
-            serverChatView.Text = String.Empty;
+			serverChatView.Text = String.Empty;
 			if (string.IsNullOrEmpty(serverPortTxt.Text))
 			{
 				MessageBox.Show("Vui lòng nhập Port và thử lại", "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
@@ -35,6 +38,77 @@ namespace Server
 				MessageBox.Show("Số port phải lớn hơn 0 và nhỏ hơn hoặc bằng 65535", "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
 				return;
 			}
+			_server = new STcpServer($"*:{portNumber}");
+			try
+			{
+				_server.StartAsync();
+				serverStartButton.Enabled = false;
+				serverStopButton.Enabled = true;
+				_updateStatusDelegate = new UpdateStatusDelegate(this.UpdateStatus);
+				this?.Invoke(_updateStatusDelegate, new object[] { $"Đang nghe tại port {portNumber}" });
+			}
+			catch (Exception ex)
+			{
+				MessageBox.Show("Load Error: " + ex.Message, "TcpServer", MessageBoxButtons.OK, MessageBoxIcon.Error);
+			}
+		}
+
+		private void serverStopButton_Click(object sender, EventArgs e)
+		{
+			_server.Stop();
+			_server = null;
+			serverStartButton.Enabled = true;
+			serverStopButton.Enabled = false;
+			this.Invoke(_updateStatusDelegate, new object[] { "Dừng server" });
+		}
+
+		private void DataReceived(object sender, DataReceivedEventArgs e)
+		{
+			Object packet = Common.ArraySegmentToObject(e.Data);
+			if (packet is UserConnectionPacket ucp)
+			{
+				_usernames.TryAdd(e.IpPort, ucp.Username);
+				SendToClient(e.Data);
+				this.Invoke(_updateStatusDelegate, new Object[] { $"---- {ucp.Username} vừa tham gia vào phòng chat ---" });
+			}
+			if (packet is ChatPacket chat)
+			{
+				SendToClient(e.Data);
+				this.Invoke(_updateStatusDelegate, new Object[] { $"{chat.Username} => {chat.ChatMessage}" });
+			}	
+		}
+
+		private void ClientDisconnected(object sender, ConnectionEventArgs e) 
+		{
+			if (!_usernames.TryGetValue(e.IpPort, out var username)) return;
+
+			UserConnectionPacket ucp = new UserConnectionPacket();
+
+			ucp.Username = username;
+			ucp.IsJoining = false;
+			var data = Common.ObjectToArraySegment(ucp);
+			SendToClient(data);
+			this.Invoke(_updateStatusDelegate, new Object[] { $"--- {username} thoát khỏi phòng chat ---" });
+		}
+
+		private void SendToClient(ArraySegment<byte> data)
+		{
+			try
+			{
+				foreach (string ipPort in _server.GetClients())
+				{
+					_ = _server.SendAsync(ipPort, data.ToArray());
+				}
+			}
+			catch (Exception ex)
+			{
+				MessageBox.Show("Send Error: " + ex.Message, "Tcp Server", MessageBoxButtons.OK, MessageBoxIcon.Error);
+			}
+		}
+
+		private void UpdateStatus(string status)
+		{
+			serverChatView.Text += status + Environment.NewLine;
 		}
 	}
 }
